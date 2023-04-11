aed2d12bcd64071e89366282a289bfee7156f20d
Samuel Elias <99209614+SamuelMonstewe@users.noreply.github.com>
2023-04-11 14:39:34 -0300

    pegando-dados-do-formulário

--- cadastro/Cliente.cs
+++ cadastro/Cliente.cs
@@ -8,5 +8,33 @@ namespace cadastro
 {
     internal class Cliente
     {
+        string Nome { get; set; }
+        string Telefone { get; set; }
+        string Endereco { get; set; }
+        string Escolar { get; set; }
+        int Idade { get; set; }
+        string Turno { get; set; }
+
+
+      
+        public void cadastrar(string nome, string telefone, string endereco, int idade, string turno)
+        {
+            nome = Nome;
+            telefone = Telefone;
+            endereco = Endereco;
+            idade = Idade;
+            turno = Turno;
+
+            
+
+        }
+        public void buscarEscolar()
+        {
+
+        }
+        public void editarPerfil()
+        {
+
+        }
     }
 }
