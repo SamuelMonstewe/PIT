0ad281cee2160d121fd78ff383fd2735b3a97a3e
SamuelMonstewe <samuelelias563@gmail.com>
2023-04-21 14:32:33 -0300

    formulario-aluno-pronto

--- cadastro/frmAluno.Designer.cs
+++ cadastro/frmAluno.Designer.cs
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlCabecalho = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEscola = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCadastar = new System.Windows.Forms.Button();
+            this.pnlRodape = new System.Windows.Forms.Panel();
+            this.pnlCabecalho.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlCabecalho
+            // 
+            this.pnlCabecalho.BackColor = System.Drawing.Color.Gold;
+            this.pnlCabecalho.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlCabecalho.Controls.Add(this.label1);
+            this.pnlCabecalho.Location = new System.Drawing.Point(-2, 0);
+            this.pnlCabecalho.Name = "pnlCabecalho";
+            this.pnlCabecalho.Size = new System.Drawing.Size(338, 75);
+            this.pnlCabecalho.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Aluno";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnCadastar);
+            this.panel1.Controls.Add(this.txtEscola);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtEndereco);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtCpf);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtIdade);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-2, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 503);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite seu nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(34, 48);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(263, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(34, 135);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(263, 20);
+            this.txtIdade.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Digite sua idade:";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(34, 220);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(263, 20);
+            this.txtCpf.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Digite o cpf:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(34, 298);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(263, 20);
+            this.txtEndereco.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Digite o endereço:";
+            // 
+            // txtEscola
+            // 
+            this.txtEscola.Location = new System.Drawing.Point(34, 375);
+            this.txtEscola.Name = "txtEscola";
+            this.txtEscola.Size = new System.Drawing.Size(263, 20);
+            this.txtEscola.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Escola onde estuda:";
+            // 
+            // btnCadastar
+            // 
+            this.btnCadastar.Location = new System.Drawing.Point(34, 431);
+            this.btnCadastar.Name = "btnCadastar";
+            this.btnCadastar.Size = new System.Drawing.Size(262, 51);
+            this.btnCadastar.TabIndex = 10;
+            this.btnCadastar.Text = "Cadastrar";
+            this.btnCadastar.UseVisualStyleBackColor = true;
+            this.btnCadastar.Click += new System.EventHandler(this.btnCadastar_Click);
+            // 
+            // pnlRodape
+            // 
+            this.pnlRodape.BackColor = System.Drawing.Color.Gold;
+            this.pnlRodape.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlRodape.Location = new System.Drawing.Point(-2, 579);
+            this.pnlRodape.Name = "pnlRodape";
+            this.pnlRodape.Size = new System.Drawing.Size(338, 88);
+            this.pnlRodape.TabIndex = 1;
+            // 
+            // frmAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(333, 666);
+            this.Controls.Add(this.pnlRodape);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlCabecalho);
+            this.Name = "frmAluno";
             this.Text = "frmAluno";
+            this.pnlCabecalho.ResumeLayout(false);
+            this.pnlCabecalho.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlCabecalho;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEscola;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCadastar;
+        private System.Windows.Forms.Panel pnlRodape;
     }
 }
