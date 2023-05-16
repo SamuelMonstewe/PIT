58295a5b495d646dd2bc1e7b8083641b1faf59d9
SamuelMonstewe <samuelelias563@gmail.com>
2023-05-16 08:53:14 -0300

    pegando-dados-com-get

--- php/index.php
+++ php/index.php
@@ -6,13 +6,13 @@ if(isset($_POST['cadastrar']))
 {
     $objMotorista->set("Cpf",$_POST['cpf']);
     $objMotorista->set("Nome",$_POST['nome']);
-    $objMotorista->set("Telefone",$_POST['telefone']);
-    $objMotorista->set("Rota",$_POST['rotas']);
-    $objMotorista->set("Turno",$_POST['turno']);
     $objMotorista->set("Idade",$_POST['idade']);
+    $objMotorista->set("Telefone",$_POST['telefone']);
+    $objMotorista->set("Turno",$_POST['turno']);
+    $objMotorista->set("Escolas",$_POST['escolas']);
 
-    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista(cpf, nome, idade, telefone, turno) VALUES(?, ?, ?)");
-    $Insert->execute([$obj->get("Cpf"), $obj->get("Nome"), $obj->get("Telefone"), $obj->get("Turno")]);
+    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista(cpf, nome, idade, telefone, turno) VALUES(?, ?, ?, ?, ?, ?, )");
+    $Insert->execute([$obj->get("Cpf"), $obj->get("Nome"), $obj->get("Telefone"), $obj->get("Idade"), , $obj->get("Telefone"), $obj->get("Turno"), $obj->get("Escolas")]);
 
 }
 
