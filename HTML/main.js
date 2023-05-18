108519f727a22b929ff7a2f95efd23fe7bcc8c8d
VictorEykel <vectoreykelmine@gmail.com>
2023-05-18 20:30:16 -0300

    Js-Com-Mascara-Telefone

--- HTML/main.js
+++ HTML/main.js
@@ -1,20 +1,47 @@
-const CaixaText = document.getElementsByTagName("input")
-
-if (CaixaText.value == "") {
-    alert("Preencha todos os campos para realizar a ação!")
-    console.log("não registrado");
+function MudarPag1() {
+    window.location.href = 'index.html';
 }
-else {
 
-    function MudarPag1() {
-        window.location.href = 'index.html';
+function MudarPag2() {
+    window.location.href = 'DadosVan.html';
+}
+
+function MudarPag3() {
+    window.location.href = 'DadosMotorista.html';
+}
+
+function MasacaraTelefone() {
+    //limitador de Caracteres
+    var tel = document.getElementById("telefone").value
+    console.log(tel)
+    tel = tel.slice(0, 13)
+    console.log(tel)
+    document.getElementById("telefone").value = tel
+    tel = document.getElementById("telefone").value.slice(0, 10)
+    console.log(tel)
+
+    //Mascara
+    var tel_formatado = document.getElementById("telefone").value
+    if (tel_formatado[0] != "(") {
+        if (tel_formatado[0] != undefined) {
+            document.getElementById("telefone").value = "(" + tel_formatado[0];
+        }
     }
 
-    function MudarPag2() {
-        window.location.href = 'DadosVan.html';
+    if (tel_formatado[3] != ")") {
+        if (tel_formatado[3] != undefined) {
+            document.getElementById("telefone").value = tel_formatado.slice(0, 3) + ")" + tel_formatado[3]
+        }
+    }
+    if (tel_formatado[4] != " ") {
+        if (tel_formatado[4] != undefined) {
+            document.getElementById("telefone").value = tel_formatado.slice(0, 4) + " " + tel_formatado[4]
+        }
     }
 
-    function MudarPag3() {
-        window.location.href = 'DadosMotorista.html';
+    if (tel_formatado[9] != "-") {
+        if (tel_formatado[9] != undefined) {
+            document.getElementById("telefone").value = tel_formatado.slice(0, 9) + "-" + tel_formatado[9]
+        }
     }
 }
