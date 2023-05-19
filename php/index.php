068e557b4bde0bc5f8dd295b31fd517cb76a6fbd
SamuelMonstewe <samuelelias563@gmail.com>
2023-05-19 09:29:04 -0300

    fazendo-upload-de-arquivos

--- php/index.php
+++ php/index.php
@@ -1,11 +1,23 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
 require_once "classes.php";
 $objMotorista = new Motorista();
 $Insert;
 
-if(isset($_POST['cadastro']))
+$DiretorioDeUpload = 'D:\wampserver\www\PIT\imagens\\';
+$FotoMotorista = $DiretorioDeUpload . basename($_FILES['foto']['name']);
+$CarteiraMotorista = $DiretorioDeUpload . basename($_FILES['carteira']['name']);
+$Crlv = $DiretorioDeUpload . basename($_FILES['crlv']['name']);
+
+if(move_uploaded_file($_FILES['foto']['tmp_name'], $FotoMotorista) && move_uploaded_file($_FILES['carteira']['tmp_name'], $CarteiraMotorista) && move_uploaded_file($_FILES['crlv']['tmp_name'], $Crlv))
 {
+    print_r($_FILES);
+}
+
+function pegarDadosDoFormulario()
+{
+    global $objMotorista;
     $objMotorista->set("Cpf", $_POST['cpf']);
     $objMotorista->set("Nome", $_POST['nome']);
     $objMotorista->set("Idade", $_POST['idade']);
@@ -15,8 +27,19 @@ if(isset($_POST['cadastro']))
     $objMotorista->set("Rota", $_POST['rota']);
     $objMotorista->set("Sexo", $_POST['sexo']);
 
-    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista(cpf, nome, idade, telefone, turno, escolas, rota, sexo) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+}
+function inserirDadosNoBanco()
+{
+    global $objMotorista;
+    global $ConexaoBanco;
+    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
     $Insert->execute([$objMotorista->get("Cpf"), $objMotorista->get("Nome"), $objMotorista->get("Idade"), $objMotorista->get("Telefone"),  $objMotorista->get("Turno"), $objMotorista->get("Escolas"), $objMotorista->get("Rota"), $objMotorista->get("Sexo")]);
+}
+
+if(isset($_POST['enviar']))
+{
+    pegarDadosDoFormulario();
+    inserirDadosNoBanco();
 
 }
 
