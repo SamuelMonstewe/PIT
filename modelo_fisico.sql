b8eca4ca71cac850e6c5736f3ff3c89f7f428e4d
VictorEykel <vectoreykelmine@gmail.com>
2023-05-23 20:03:28 -0300

    modelo_fiisco-atualizado

--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -2,49 +2,33 @@ create database pit;
 
 use pit;
 
+create table motorista(
+cpf char(14) not null primary key,
+nome varchar(80) not null,
+idade tinyint not null,
+telefone char(14) not null,
+turno varchar(12) not null,
+escola_fk int not null,
+rota varchar(45) not null,
+sexo char(1) not null
+);
+
 select * from motorista;
 delete from motorista where cpf = '498.327.439-24';
 alter table motorista drop column endereco;
 alter table motorista add column rota varchar(45) not null;
 delete from motorista where cpf = '3424';
 
-CREATE TABLE `escola_motorista` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `escolas_id` int DEFAULT NULL,
-  `id_motorista` varchar(14) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table escola_motorista (
+	id int auto_increment primary key,
+    escolas_id int,
+    cpf_motorista varchar(14)
+);
 
-CREATE TABLE `escolas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `motorista` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cpf` char(14) NOT NULL,
-  `nome` varchar(80) NOT NULL,
-  `idade` tinyint NOT NULL,
-  `telefone` char(14) NOT NULL,
-  `turno` varchar(12) NOT NULL,
-  `rota` varchar(45) NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `fotoMotorista` longtext,
-  `fotoCarteira` longtext,
-  `fotoCRLV` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Usuario` varchar(80) DEFAULT NULL,
-  `email` varchar(40) DEFAULT NULL,
-  `senha` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Usuario_UNIQUE` (`Usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+create table escolas (
+	id int auto_increment primary key,
+    nome varchar(80) not null
+);
 
 create table van(
 chassi char(17) not null primary key,
