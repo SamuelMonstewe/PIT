09f872f4a40c31c873896716687306a04ed75de1
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-19 08:41:47 -0300

    implementando-tratamento-de-erro-e-login

--- php/login.php
+++ php/login.php
@@ -1,31 +1,43 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 require_once "pdo.php";
 require_once "classes.php";
 
-$usuario = new Usuario();
-function Logar(){
-    global $ConexaoBanco;
-    global $usuario;
-    $usuario->Usuario =$_POST['usuario'];
-    $usuario->Senha=$_POST['senha'];
-    $select = $ConexaoBanco->query("select * from pit.usuarios where usuario = '$usuario->Usuario' and senha = '$usuario->Senha'");
-    $checar = $select->fetchAll();
 
-    if(isset($checar[0])){
-        echo(json_encode($checar[0]));
+function Logar()
+{
+
+    global $ConexaoBanco;
+    $Usuario = $_POST['usuario'];
+    $Senha = $_POST['senha'];
+
+    try {
+       
+        $select = $ConexaoBanco->prepare("select * from usuarios where Usuario = :Usuario and senha = :Senha");
+        $select->bindParam(':Usuario', $Usuario);
+        $select->bindParam(':Senha', $Senha);
+        $select->execute();
+        $checar = $select->fetch(PDO::FETCH_ASSOC);
+
+        if ($checar) {
+            echo json_encode($checar);
+        } 
+        else {
+            http_response_code(401);
+            throw new Exception("Senha inválida!");
+        }
+
     }
-    else
-    {
-        http_response_code(401);
-        echo("Senha invalida!");
+    catch(Exception $e){
+        echo json_encode(array("erro" => $e->getMessage()));
+    }
+    finally{
+        $ConexaoBanco = null;
     }
 
 }
-
-if(isset($_POST['enviar']))
-{
+if (isset($_POST['enviar'])) {
     Logar();
 }
 ?>
