211ed7aab336330f16afe71a1eb8ca330440fb0f
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-19 09:26:11 -0300

    implementando-tratamento-de-erro

--- php/cadastro.php
+++ php/cadastro.php
@@ -6,24 +6,57 @@ require_once "classes.php";
 
 $usuario = new Usuario();
 
-function PreencherDados(){
-    global $usuario;    
-    $usuario->Usuario = $_POST['usuario'];
-    $usuario->Email = $_POST['email'];
-    $usuario->Senha = $_POST['senha'];
+function PreencherDados()
+{
+    global $usuario;
+    $usuario->Usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
+    $usuario->Email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $usuario->Senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+
 
 }
-function InserirDados() {
+function InserirDados()
+{
     global $usuario;
     global $ConexaoBanco;
 
-    $Insert = $ConexaoBanco->prepare("INSERT INTO usuarios VALUES (null,? ,? ,?)");
-    $Insert->execute([$usuario->Usuario,$usuario->Email,$usuario->Senha]);
+    try {
+
+
+        $query = $ConexaoBanco->prepare("SELECT COUNT(*) FROM usuarios WHERE email = :email");
+        $query->bindParam(':email', $usuario->Email);
+        $query->execute();
+        $result = $query->fetchColumn();
+
+        if ($result > 0) {
+            echo json_encode(array("erro" => "O email já está cadastrado."));
+        } 
+        else {
+            $Insert = $ConexaoBanco->prepare("INSERT INTO usuarios VALUES (null,:Usuario ,:Email ,:Senha)");
+            $Insert->bindParam(':Usuario', $usuario->Usuario);
+            $Insert->bindParam(':Email', $usuario->Email);
+            $Insert->bindParam(':Senha', $usuario->Senha);
+
+            $Insert->execute();
+            $linhasAfetadas = $Insert->rowCount();
+            if ($linhasAfetadas > 0) {
+                echo json_encode(array("mensagem" => "Inserção bem-sucedida!"));
+
+            } else {
+                throw new Exception("Erro ao inserir os dados.");
+            }
+        }
+
+    } catch (PDOException $e) {
+        echo json_encode(array("erro" => $e->getMessage()));
+    } finally {
+        $ConexaoBanco = null;
+    }
 
 }
 
-if(isset($_POST['enviar']))
-{
+if (isset($_POST['enviar'])) {
     PreencherDados();
     InserirDados();
 }
