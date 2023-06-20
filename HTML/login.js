ff24fbdd7c28c8013c664d26152f6dfee9548fbe
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-20 08:46:19 -0300

    exibindo-mensagem-para-usuário

--- HTML/login.js
+++ HTML/login.js
@@ -17,11 +17,18 @@ function enviarDados() {
     http.onreadystatechange = function () {
         //verifica retorno do back-end 
         if (http.readyState == 4 && http.status == 200) {
-            var login = JSON.parse(http.response)
-            mudarpagina()
+            // var login = JSON.parse(http.response)
+            var verificacao = confirm("login realizado com sucesso! Deseja ir para dados de motorista?")
+            if(verificacao == true)
+            {
+                mudarPagina()
+            }
+            else{
+                return
+            }
         }
         else if (http.readyState == 4 && http.status == 401)
-        {
+        {   alert("senha inválida")
             return
         }
         else if (http.readyState == 4 && http.status != 200) {
