7f252e92fc9f0008e0f292d648e6a46c0183a601
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-21 08:18:23 -0300

    removendo-chave-do-banco-no-email-confirmado

--- php/confirmar-email.php
+++ php/confirmar-email.php
@@ -8,26 +8,34 @@ $chave = filter_input(INPUT_GET, "chave", FILTER_SANITIZE_STRING);
 if(!empty($chave)){
 
     global $ConexaoBanco;
-    $query_usuarios = "SELECT id FROM usuarios WHERE chave = :chave LIMIT 1";
-    $result_usuario = $ConexaoBanco->prepare($query_usuarios);
-    $result_usuario-> bindParam(':chave', $chave);
-    $result_usuario->execute();
+    $SELECT = "SELECT id FROM usuarios WHERE chave = :chave LIMIT 1";
+    $resultadoDaConsulta = $ConexaoBanco->prepare($SELECT);
+    $resultadoDaConsulta-> bindParam(':chave', $chave);
+    $resultadoDaConsulta->execute();
 
-    if(($result_usuario) and ($result_usuario->rowCount() != 0)){
-        $_SESSION['mensagem'] = "<div class='alert alert-success' role='alert'> Email confirmado </div>";
-        header("Location: ../php/cadastro.php");
+    if(($resultadoDaConsulta) and ($resultadoDaConsulta->rowCount() != 0)){
+        $linhaRetornada = $resultadoDaConsulta->fetch(PDO::FETCH_ASSOC);
+        extract($linhaRetornada);
+      
+        $updateUsuario = "UPDATE usuarios SET sits_usuarios_id = 1, chave=:chave WHERE id = " . strval($id);
+        $update = $ConexaoBanco->prepare($updateUsuario);
+        $chave = NULL;
+        $update->bindParam(':chave', $chave);
+        if($update->execute()){
+            $_SESSION['mensagem'] = "<div class='alert alert-success' role='alert'> Email confirmado </div>";
+            header("Location: ../php/cadastro.php");
+        }
+        else{
+            $_SESSION['mensagem'] = "<div class='alert alert-danger' role='alert'> Erro: Email nao confirmado.</div> ";
+            header("Location: ../php/cadastro.php");      
+        }
     }
     else{
         $_SESSION['mensagem'] = "<div class='alert alert-danger' role='alert'> Erro: Endereço inválido.</div> ";
         header("Location: ../php/cadastro.php");
     }
-
 }
 else{
     $_SESSION['mensagem'] = "<div class='alert alert-danger' role='alert'> Erro: Endereço inválido.</div> ";
     header("Location: ../php/cadastro.php");
 }
-// else{
-//     $_SESSION['mensagem'] = 
-//     header("Location: ../php/cadastro.php");
-// }
