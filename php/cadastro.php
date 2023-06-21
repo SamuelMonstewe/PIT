5afcc4b120917908676a86db93328f66f58e5e3e
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-21 08:43:01 -0300

    implementando-html-para-exibição

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
@@ -35,7 +36,7 @@ function InserirDados()
         } 
         else {
 
-            $Insert = $ConexaoBanco->prepare("INSERT INTO usuarios VALUES (null,:Usuario ,:Senha ,:Chave ,:Email)");
+            $Insert = $ConexaoBanco->prepare("INSERT INTO usuarios VALUES (null,:Usuario ,:Email ,:Senha ,:Chave, 3)");
             $Insert->bindParam(':Usuario', $usuario->Usuario);
             $Insert->bindParam(':Email', $usuario->Email);
             $Insert->bindParam(':Senha', $usuario->Senha);
@@ -67,9 +68,9 @@ function InserirDados()
                     $mail->isHTML(true); 
                     $mail->Subject = 'CONFIRMAR O EMAIL';
                     $mail->Body = "Por favor confirme o email <br> 
-                    <a href='http://localhost/PIT/confirmar-email.php?chave='>Clique aqui </a>";
+                    <a href='http://localhost/PIT/php/confirmar-email.php?chave=$chave'>Clique aqui </a>";
                     $mail->AltBody = "Por favor confirme o email \n
-                    'http://localhost/PIT/confirmar-email.php?chave='";
+                    'http://localhost/PIT/php/confirmar-email.php?chave=$chave'";
 
                     $mail->send();
 
@@ -92,10 +93,31 @@ function InserirDados()
     }
 
 }
-
+if(isset($_SESSION['mensagem'])){
+    echo $_SESSION['mensagem'];
+    unset($_SESSION['mensagem']);
+}
 if (isset($_POST['enviar'])) {
     PreencherDados();
     InserirDados();
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+<script> src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+</html>
