7433aff06d8be7a6b8d7ef7b2eb67312f3f7a224
SamuelMonstewe <samuelelias563@gmail.com>
2023-06-26 09:46:10 -0300

    redefinindo-a-senha

--- php/redefinir-senha.php
+++ php/redefinir-senha.php
@@ -1,9 +1,25 @@
 <?php 
 require_once "pdo.php";
 function redefinirSenha(){
+global $ConexaoBanco;
+$email = $_POST['email'];
 $senha = $_POST['senhanova'];
 $senhaConfirmacao = $_POST['confirmacaosenha'];
 
+if($senha != $senhaConfirmacao){
+    $mensagem = "Digite senhas iguais!";
+    echo json_encode($mensagem);
+}
+else{
+    $UPDATE = $ConexaoBanco->prepare("UPDATE usuarios SET senha = :senha WHERE email = :email");
+    $UPDATE->bindParam(':senha', $senha);
+    $UPDATE->bindParam(':email', $email);
+    if($UPDATE->execute()){
+        $mensagem = "Senha redefinida com sucesso!";
+        echo json_encode($mensagem);
+    }
+   
+}
 }
 
 if(isset($_POST['enviar'])){
