0c8bd54cc92ce1cc1f0cf40795fc7ba57aaa1292
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 10:29:01 -0300

    corrigindo bindparam

--- php/dadosvan.php
+++ php/dadosvan.php
@@ -2,42 +2,65 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
-require_once "classes.php";
+require_once "classes/Van.php";
 
-$van = new DadosVan();
+$van = new Van();
 
 function PegarDadosVan()
 {
     global $van;
-    $van->set("Chassi", $_POST['chassi']);
-    $van->set("Placa", $_POST['placaVeiculo']);
-    $van->set("Marca", $_POST['marca']);
-    $van->set("Modelo", $_POST['modelo']);
-    $van->set("Quantlugar", $_POST['capacidadeAlunos']);
-    $van->set("LaudoInspecaoVeicular", base64_encode(file_get_contents($_FILES['laudo']['tmp_name'])));
-    $van->set("FotoInterna", base64_encode(file_get_contents($_FILES['fotoInterna']['tmp_name'])));
-    $van->set("FotoExterna", base64_encode(file_get_contents($_FILES['fotoExterna']['tmp_name'])));
+    $van->setChassi($_POST['chassi']);
+    $van->setPlaca($_POST['placaVeiculo']);
+    $van->setMarca($_POST['marca']);
+    $van->setModelo($_POST['modelo']);
+    $van->setQuantLugar($_POST['capacidadeAlunos']);
+    $van->setLaudoInspecaoVeicular(base64_encode(file_get_contents($_FILES['laudo']['tmp_name'])));
+    $van->setFotoInterna(base64_encode(file_get_contents($_FILES['fotoInterna']['tmp_name'])));
+    $van->setFotoExterna(base64_encode(file_get_contents($_FILES['fotoExterna']['tmp_name'])));
+    
 }
 
-function InserirDadosNoBanco()
+function InserirDados()
 {
-    global $van;
-    global $ConexaoBanco;
-    $insert = $ConexaoBanco->prepare("INSERT INTO van VALUES (?, ?, ?, ?, ?, null, ?, ?, ?)");
-    $insert->execute([
-        $van->get("Chassi"),  $van->get("Placa"), $van->get("Marca"), $van->get("Modelo"), $van->get("Quantlugar"),
-        $van->get("LaudoInspecaoVeicular"), $van->get("FotoInterna"), $van->get("FotoExterna")
-    ]);
+    try{
+        global $van;
+        global $ConexaoBanco;
+        $insert = $ConexaoBanco->prepare("INSERT INTO van (chassi, placa, marca, modelo, qtd_lugares, laudo_inspecao_veicular, foto_interna, foto_externa) 
+                                          VALUES (:chassi, :placa, :marca, :modelo, :qtd_lugares, :laudo_inspecao_veicular, :foto_interna, :foto_externa)");
+
+        $chassi = $van->getChassi();
+        $placa  =  $van->getPlaca();
+        $marca = $van->getMarca();
+        $modelo = $van->getModelo();
+        $quantidadeDeLugar =  $van->getQuantlugar();
+        $laudoInspecaoVeicular = $van->getLaudoInspecaoVeicular();
+        $fotoInterna = $van->getFotoInterna();
+        $fotoExterna = $van->getFotoExterna();
+
+        $insert->bindParam(':chassi',  $chassi);
+        $insert->bindParam(':placa', $placa);
+        $insert->bindParam(':marca',  $marca);
+        $insert->bindParam(':modelo',  $modelo);
+        $insert->bindParam(':qtd_lugares', $quantidadeDeLugar);
+        $insert->bindParam(':laudo_inspecao_veicular', $laudoInspecaoVeicular);
+        $insert->bindParam(':foto_interna', $fotoInterna);
+        $insert->bindParam(':foto_externa', $fotoExterna);
+        $insert->execute();
+    }
+    catch(PDOException $e){
+        echo ($e->getMessage());
+    }
 }
 
 if (isset($_POST['enviar']))
 {
     PegarDadosVan();
-    InserirDadosNoBanco();
-    echo("foi ss ó");
+    InserirDados();
 }
 else 
 {
     http_response_code(400);
     echo("dados incompletos");
+  
 }
+?>
