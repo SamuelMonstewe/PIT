8abdd60376637e55e757f1861aedc1669e5faf33
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 08:50:12 -0300

    nova classe para van

---
+++ php/classes/Van.php
@@ -0,0 +1,101 @@
+<?php
+
+
+class Van {
+    private $Chassi;
+    private $Placa;
+    private $Marca;
+    private $Modelo;
+    private $Quantlugar;
+    private $LaudoInspecaoVeicular;
+    private $FotoInterna;
+    private $FotoExterna;
+  
+
+	
+	public function getChassi() {
+		return $this->Chassi;
+	}
+	
+	
+	public function setChassi($Chassi) {
+		$this->Chassi = $Chassi;
+		
+	}
+	
+	
+	public function getPlaca() {
+		return $this->Placa;
+	}
+	
+	
+	public function setPlaca($Placa) {
+		$this->Placa = $Placa;
+	
+	}
+	
+	
+	public function getMarca() {
+		return $this->Marca;
+	}
+	
+
+	public function setMarca($Marca) {
+		$this->Marca = $Marca;
+		
+	}
+	
+	
+	public function getModelo() {
+		return $this->Modelo;
+	}
+	
+
+	public function setModelo($Modelo) {
+		$this->Modelo = $Modelo;
+		
+	}
+	
+	
+	public function getQuantlugar() {
+		return $this->Quantlugar;
+	}
+	
+
+	public function setQuantlugar($Quantlugar) {
+		$this->Quantlugar = $Quantlugar;
+	
+	}
+	
+	
+	public function getLaudoInspecaoVeicular() {
+		return $this->LaudoInspecaoVeicular;
+	}
+	
+	
+	public function setLaudoInspecaoVeicular($LaudoInspecaoVeicular) {
+		$this->LaudoInspecaoVeicular = $LaudoInspecaoVeicular;
+		
+	}
+	
+
+	public function getFotoInterna() {
+		return $this->FotoInterna;
+	}
+	
+
+	public function setFotoInterna($FotoInterna) {
+		$this->FotoInterna = $FotoInterna;
+		
+	}
+
+	public function getFotoExterna() {
+		return $this->FotoExterna;
+	}
+	
+	
+	public function setFotoExterna($FotoExterna) {
+		$this->FotoExterna = $FotoExterna;
+		
+	}
+}
