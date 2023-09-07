40281071bcb4269ccfeef60c61ca13f95f1f53b6
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 10:28:08 -0300

    corrigindo bindparam

--- php/cadastroCliente.php
+++ php/cadastroCliente.php
@@ -3,45 +3,50 @@ session_start();
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
-require_once "classes.php";
+require_once "classes/Usuario.php";
 require_once "verificarUsuarioExistente.php";
 require_once "emailDeConfirmacao.php";
 require_once "gerarChave.php";
 
 require '../lib/vendor/autoload.php';
-
+$usuario = new Usuario();
 
 function InserirDadosCliente()
 {
     global $usuario;
     global $ConexaoBanco;
-    $usuario = new Usuario();
-    $usuario->Usuario = filter_input(INPUT_POST, 'usuario');
-    $usuario->Email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    $usuario->Senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
-
+   
+    $usuario->setNomeUsuario($_POST['usuario']);
+    $usuario->setEmail($_POST['email']);
+    $usuario->setSenha($_POST['senha']);
+    $usuario->setChave(GerarChave());
     try {
 
-        if (VerificarSeUsuarioExiste($usuario->Usuario, $usuario->Email)) {
+        if (VerificarSeUsuarioExiste($usuario->getNomeUsuario(), $usuario->getEmail())) {
             $mensagem = "O email já está cadastrado!";
             echo json_encode($mensagem);
             exit;
         } 
         else {
             $Insert = $ConexaoBanco->prepare("INSERT INTO usuarios VALUES (null,:Usuario ,:Email ,:Senha ,:Chave, 3, 2)");
-            $Insert->bindParam(':Usuario', $usuario->Usuario);
-            $Insert->bindParam(':Email', $usuario->Email);
-            $Insert->bindParam(':Senha', $usuario->Senha);
-            $usuario->Chave = GerarChave();
-            $Insert->bindParam(':Chave', $usuario->Chave);
+
+            $nomeUsuario = $usuario->getNomeUsuario();
+            $emailUsuario = $usuario->getEmail();
+            $senhaUsuario = $usuario->getSenha();
+            $chaveUsuario = $usuario->getChave();
+            
+            $Insert->bindParam(':Usuario', $nomeUsuario);
+            $Insert->bindParam(':Email', $emailUsuario);
+            $Insert->bindParam(':Senha', $senhaUsuario);
+            $Insert->bindParam(':Chave', $chaveUsuario);
             $Insert->execute();
             $linhasAfetadas = $Insert->rowCount();
 
             if ($linhasAfetadas > 0) {
                 $mensagem = "Enviamos um email para confirmação!";
                 echo json_encode($mensagem);
-                EnviarEmailDeConfirmacao();
-                
+                EnviarEmailDeConfirmacao($usuario);
+                exit;
             } 
             else {
                 throw new Exception("Erro ao inserir os dados.");
