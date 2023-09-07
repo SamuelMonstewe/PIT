a0417643fdf58be4b4ae3ce5205b6aa4485f2ef1
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 08:48:53 -0300

    usando novos métodos de leitura e escrita

--- php/dadosmotorista.php
+++ php/dadosmotorista.php
@@ -2,40 +2,43 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
-require_once "classes.php";
-$objMotorista = new Motorista();
+require_once "classes/Motorista.php";
+$motorista = new Motorista();
 
 function pegarDadosDoFormulario()
 {
-    global $objMotorista;
-    $objMotorista->set("Cpf", $_POST['cpf']);
-    $objMotorista->set("Nome", $_POST['nome']);
-    $objMotorista->set("Idade", $_POST['idade']);
-    $objMotorista->set("Telefone", $_POST['telefone']);
-    $objMotorista->set("Turno", $_POST['turno']);
-    $objMotorista->set("Escolas", $_POST['idescola']);
-    $objMotorista->set("Rota", $_POST['rota']);
-    $objMotorista->set("Sexo", $_POST['sexo']);
-    $objMotorista->set("fotoMotorista", base64_encode(file_get_contents($_FILES['fotomotorista']['tmp_name'])));
-    $objMotorista->set("fotoCarteira", base64_encode(file_get_contents($_FILES['fotocarteira']['tmp_name'])));
-    $objMotorista->set("fotoCRLV", base64_encode(file_get_contents($_FILES['fotocrlv']['tmp_name'])));
+    global $motorista;
+    $motorista->setCpf($_POST['cpf']);
+    $motorista->setNome($_POST['nome']);
+    $motorista->setIdade($_POST['idade']);
+    $motorista->setTelefone($_POST['telefone']);
+    $motorista->setTurno($_POST['turno']);
+    $motorista->setEscolas($_POST['idescola']);
+    $motorista->setRota($_POST['rota']);
+    $motorista->setSexo($_POST['sexo']);
+    $motorista->setFotoMotorista(base64_encode(file_get_contents($_FILES['fotomotorista']['tmp_name'])));
+    $motorista->setFotoCarteira(base64_encode(file_get_contents($_FILES['fotocarteira']['tmp_name'])));
+    $motorista->setFotoCRLV(base64_encode(file_get_contents($_FILES['fotocrlv']['tmp_name'])));
 }
 function inserirDadosNoBanco()
 {
-    global $objMotorista;
+    global $motorista;
     global $ConexaoBanco;
-    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $Insert->execute([
-        $objMotorista->get("Cpf"), $objMotorista->get("Nome"), $objMotorista->get("Idade"),
-        $objMotorista->get("Telefone"),  $objMotorista->get("Turno"), $objMotorista->get("Rota"),
-        $objMotorista->get("Sexo"), $objMotorista->get("fotoMotorista"), $objMotorista->get("fotoCarteira"),
-        $objMotorista->get("fotoCRLV")
-    ]);
-    $idMotorista = $ConexaoBanco->lastInsertId();
-    $Insert2 = $ConexaoBanco->prepare("INSERT INTO escola_motorista VALUES (null,?,?)");
-    $Insert2->execute([
-        $objMotorista->get("Escolas"), $idMotorista
-    ]);
+    $Insert = $ConexaoBanco->prepare("INSERT INTO motorista VALUES(null, :cpf, :nome, :idade, :telefone, :turno, :rota, :sexo, :fotoMotorista, :fotoCarteira, :fotoCRLV)");
+    $Insert->bindParam(':cpf', $motorista->getCpf());
+    $Insert->bindParam(':nome', $motorista->getNome());
+    $Insert->bindParam(':idade', $motorista->getIdade());
+    $Insert->bindParam(':telefone', $motorista->getTelefone());
+    $Insert->bindParam(':turno', $motorista->getTurno());
+    $Insert->bindParam(':rota', $motorista->getRota());
+    $Insert->bindParam(':sexo', $motorista->getSexo());
+    $Insert->bindParam(':fotoMotorista', $motorista->getFotoMotorista());
+    $Insert->bindParam(':fotoCarteira', $motorista->getFotoCarteira());
+    $Insert->bindParam(':fotoCRLV', $motorista->getFotoCRLV());
+    $Insert->execute();
+    // $idMotorista = $ConexaoBanco->lastInsertId();
+    // $Insert2 = $ConexaoBanco->prepare("INSERT INTO escola_motorista VALUES (null,?,?)");
+   
 }
 
 if (isset($_POST['enviar'])) {
