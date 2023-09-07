0664ad4dafb2911a1a82ce946c63ed37c54c9638
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 10:57:13 -0300

    removendo include desnecessário

--- php/Perfil.php
+++ php/Perfil.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
 require_once "pdo.php";
-require_once "classes.php";
+
 
 function buscarDados()
 {
