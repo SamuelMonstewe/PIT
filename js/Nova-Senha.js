b05ba3a0a0e336ae167de2d7de2338bfde935ce6
SamuelMonstewe <samuelelias563@gmail.com>
2023-09-07 11:00:49 -0300

    pasta apenas para javascript

---
+++ js/Nova-Senha.js
@@ -0,0 +1,39 @@
+var base_url = "http://localhost/"
+
+function mudarpagina() {
+    window.location.href = "login.html"
+}
+
+function enviarDados()
+{
+    var http = new XMLHttpRequest()
+    var url = base_url + "PIT/php/redefinir-senha.php"
+ 
+    http.open("POST", url, true)
+
+    http.onreadystatechange = function () {
+        //verifica retorno do back-end 
+        if (http.readyState == 4 && http.status == 200) {
+            
+            try {
+                var response = JSON.parse(http.responseText)
+                var mensagem = response
+                alert(mensagem)
+            }
+            catch(error) {
+                console.log('Tá dando erro na conversão do json', error, http.responseText);
+            }
+        }
+        else if (http.status != 200) {
+            alert("ERRO!")
+        }
+    }
+
+    var data = new FormData()
+
+    data.append('senhanova', document.getElementById("Nova-Senha").value)
+    data.append('email', document.getElementById("email").value)
+    data.append('confirmacaosenha', document.getElementById("Confirmar-Senha").value)
+    data.append('enviar', 'enviar')
+    http.send(data)
+}
