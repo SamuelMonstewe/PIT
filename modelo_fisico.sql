252cd387fa78358183a05e0820d06aae8a2d94da
VictorEykel <vectoreykelmine@gmail.com>
2023-09-09 14:02:40 -0300

    Banco-de-Dados-Atualizado

--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -2,35 +2,6 @@ create database pit;
 
 use pit;
 
-CREATE TABLE `motorista` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cpf` char(14) NOT NULL,
-  `nome` varchar(80) NOT NULL,
-  `idade` tinyint NOT NULL,
-  `telefone` char(14) NOT NULL,
-  `turno` varchar(12) NOT NULL,
-  `rota` varchar(45) NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `fotoMotorista` longtext,
-  `fotoCarteira` longtext,
-  `fotoCRLV` longtext,
-  PRIMARY KEY (`id`)
-) ;
-
-select * from motorista;
-CREATE TABLE `escola_motorista` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `escolas_id` int DEFAULT NULL,
-  `id_motorista` varchar(14) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
-CREATE TABLE `escolas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
 CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `Usuario` varchar(80) DEFAULT NULL,
@@ -44,40 +15,51 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `Usuario_UNIQUE` (`Usuario`)
 ) ;
+CREATE TABLE `motorista` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` char(14) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `idade` tinyint(4) NOT NULL,
+  `telefone` char(14) NOT NULL,
+  `regiao_atuacao` varchar(45) NOT NULL,
+  `sexo` char(1) NOT NULL,
+  `fotoMotorista` longtext,
+  `fotoCarteira` longtext,
+  `fotoCRLV` longtext,
+  `turnoManha` varchar(10) DEFAULT NULL,
+  `turnoNoite` varchar(10) DEFAULT NULL,
+  `turnoTarde` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-select * from pit.usuarios;
-drop table usuarios;
 CREATE TABLE `van` (
+   id int not null primary key auto_increment,
   `chassi` char(17) NOT NULL,
   `placa` char(8) NOT NULL,
   `marca` varchar(30) NOT NULL,
   `modelo` varchar(30) NOT NULL,
   `qtd_lugares` tinyint NOT NULL,
-  `motorista_id_fk` int DEFAULT NULL,
+  `motorista_id_fk` int,
   `laudo_inspecao_veicular` longtext,
   `foto_interna` longtext,
   `foto_externa` longtext,
-  PRIMARY KEY (`chassi`),
-  KEY `motorista_id_fk_idx` (`motorista_id_fk`)
-) ;
-create table situacao_usuarios(
-id int auto_increment primary key,
-nome_situacao varchar(45) not null
-);
+  foreign key (motorista_id_fk) references motorista(id)
+  );
 
-create table tipo_usuarios (
-id int auto_increment primary key,
-tipo_situacao varchar(45) not null
-);
+CREATE TABLE `tipo_usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo_situacao` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-insert into situacao_usuarios values (null, "Ativo");
-insert into situacao_usuarios values (null, "Inativo");
-insert into situacao_usuarios values (null, "Aguardando confirmação");
+CREATE TABLE `situacao_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_situacao` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
-insert into tipo_usuarios values (null, "Motorista");
-insert into tipo_usuarios values (null, "Cliente");
-
-select * from situacao_usuarios;
-select * from tipo_usuarios;
-select * from van;
-
+CREATE TABLE `escolas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=177 DEFAULT CHARSET=utf8;
