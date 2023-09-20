aafb16846645110df94140a2f694037ae2e4604a
SAMUEL_ELIAS_SANTOS_OLIVEIRA <22200134@DELTAF43.FLORESTA>
2023-09-20 15:22:03 -0300

    pegando id do motorista para fk

--- php/dadosvan.php
+++ php/dadosvan.php
@@ -3,12 +3,15 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
 require_once "pdo.php";
 require_once "classes/Van.php";
+require_once "classes/Motorista.php";
 
 $van = new Van();
 
 function PegarDadosVan()
 {
     global $van;
+ 
+    $van->setCpfMotoristaDaVan($_POST['cpf']);
     $van->setChassi($_POST['chassi']);
     $van->setPlaca($_POST['placaVeiculo']);
     $van->setMarca($_POST['marca']);
@@ -17,16 +20,22 @@ function PegarDadosVan()
     $van->setLaudoInspecaoVeicular(base64_encode(file_get_contents($_FILES['laudo']['tmp_name'])));
     $van->setFotoInterna(base64_encode(file_get_contents($_FILES['fotoInterna']['tmp_name'])));
     $van->setFotoExterna(base64_encode(file_get_contents($_FILES['fotoExterna']['tmp_name'])));
-    
 }
 
 function InserirDados()
 {
-    try{
-        global $van;
+    try {
         global $ConexaoBanco;
-        $insert = $ConexaoBanco->prepare("INSERT INTO van (chassi, placa, marca, modelo, qtd_lugares, laudo_inspecao_veicular, foto_interna, foto_externa) 
-                                          VALUES (:chassi, :placa, :marca, :modelo, :qtd_lugares, :laudo_inspecao_veicular, :foto_interna, :foto_externa)");
+        global $van;
+        $insert = $ConexaoBanco->prepare("INSERT INTO van (chassi, placa, marca, modelo, qtd_lugares, motorista_id_fk,laudo_inspecao_veicular, foto_interna, foto_externa) 
+                                          VALUES (:chassi, :placa, :marca, :modelo, :qtd_lugares, :motorista_id_fk, :laudo_inspecao_veicular, :foto_interna, :foto_externa)");
+
+        $SELECT_MOTORISTA = $ConexaoBanco->prepare("SELECT id FROM motorista WHERE cpf = :cpf");
+        $cpf = $van->getCpfMotoristaDaVan();
+        $SELECT_MOTORISTA->bindParam(':cpf', $cpf);
+        $SELECT_MOTORISTA->execute();
+        $dados_retornados = $SELECT_MOTORISTA->fetch(PDO::FETCH_ASSOC);
+        $idMotorista_fk = $dados_retornados['id'];
 
         $chassi = $van->getChassi();
         $placa  =  $van->getPlaca();
@@ -36,31 +45,27 @@ function InserirDados()
         $laudoInspecaoVeicular = $van->getLaudoInspecaoVeicular();
         $fotoInterna = $van->getFotoInterna();
         $fotoExterna = $van->getFotoExterna();
-
+        
         $insert->bindParam(':chassi',  $chassi);
         $insert->bindParam(':placa', $placa);
         $insert->bindParam(':marca',  $marca);
         $insert->bindParam(':modelo',  $modelo);
         $insert->bindParam(':qtd_lugares', $quantidadeDeLugar);
+        $insert->bindParam(':motorista_id_fk', $idMotorista_fk);
         $insert->bindParam(':laudo_inspecao_veicular', $laudoInspecaoVeicular);
         $insert->bindParam(':foto_interna', $fotoInterna);
         $insert->bindParam(':foto_externa', $fotoExterna);
         $insert->execute();
-    }
-    catch(PDOException $e){
+
+    } catch (PDOException $e) {
         echo ($e->getMessage());
     }
 }
 
-if (isset($_POST['enviar']))
-{
+if (isset($_POST['enviar'])) {
     PegarDadosVan();
     InserirDados();
+} else {
+
+    echo ("dados incompletos");
 }
-else 
-{
-    http_response_code(400);
-    echo("dados incompletos");
-  
-}
-?>
