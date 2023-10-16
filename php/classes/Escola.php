4128212e86e25d92919152e59fe1dd3f6a0bea43
SamuelMonstewe <samuelelias563@gmail.com>
2023-10-16 16:10:46 -0300

    removendo propriedade desnecessária

--- php/classes/Escola.php
+++ php/classes/Escola.php
@@ -1,21 +1,9 @@
 <?php
 
 class Escola {
-    private $id;
+ 
     private $nome;
-   
-
-	
-	public function getId() {
-		return $this->id;
-	}
-	
-
-	public function setId($id) {
-		$this->id = $id;
-	
-	}
-	
+   	
 
 	public function getNome() {
 		return $this->nome;
